f7b83fc6b91ff3c99915be780fe9b2458feae984
T-ramya-sree <85395315+T-ramya-sree@users.noreply.github.com>
2026-06-03 12:30:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// Complete the js code
-function Car(make, model) {}
+// Complete the js 
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+Car.protorype.getMakeModel=function() {
+	return this._make+ " " +this.model;
+	
+};
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed=topSpeed;
+}
+SportsCar.prototype=Object.create(Car.prototype);
+SportsCar.prototype.constructor=SportsCar;
+SportsCar.prototype.getTopSpeed=function(){
+	return this.topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
